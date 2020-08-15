ffb0dd1abb03e756d25792623fc20cfcfe5a5026
DafyddLlyr <dafydd.pearson@thinkwhere.com>
2020-08-15 16:27:26 +0100

    Basic components in place

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeroComponent } from './hero/hero.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { PortfolioContainerComponent } from './portfolio-container/portfolio-container.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroComponent,
+    AboutMeComponent,
+    PortfolioContainerComponent,
+    ContactComponent,
   ],
   imports: [
     BrowserModule
---
+++ src/app/portfolio-container/portfolio-container.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-portfolio-container',
+  templateUrl: './portfolio-container.component.html',
+  styleUrls: ['./portfolio-container.component.css']
+})
+export class PortfolioContainerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
