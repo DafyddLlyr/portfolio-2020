bfa84e2f667b1abd0705c59b7d80c22363aafc28
DafyddLlyr <dafydd.pearson@thinkwhere.com>
2020-08-16 16:06:33 +0100

    Basic tech stack concept in place

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -6,6 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about-me.component.scss']
 })
 export class AboutMeComponent implements OnInit {
+  techStack = [
+    { name: 'HTML5', icon: 'postgres.png'},
+    { name: 'CSS3', icon: 'postgres.png'},
+    { name: 'JavaScript', icon: 'postgres.png'},
+    { name: 'Typescript', icon: 'postgres.png'},
+    { name: 'Python', icon: 'postgres.png'},
+    { name: 'Flask', icon: 'postgres.png'},
+    { name: 'Angular', icon: 'postgres.png'},
+    { name: 'AWS', icon: 'postgres.png'},
+    { name: 'OpenLayers', icon: 'postgres.png'},
+    { name: 'PostgreSQL', icon: 'postgres.png'},
+  ];
 
   constructor() { }
 
