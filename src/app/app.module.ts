c0c63f241a529c9ac73c5c1f7b974ad999b8b8a1
DafyddLlyr <dafydd.pearson@thinkwhere.com>
2020-08-17 17:39:49 +0100

    Start of portfolio item component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { HeroComponent } from './hero/hero.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { PortfolioContainerComponent } from './portfolio-container/portfolio-container.component';
 import { ContactComponent } from './contact/contact.component';
+import { PortfolioItemComponent } from './portfolio-container/portfolio-item/portfolio-item.component';
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import { ContactComponent } from './contact/contact.component';
     AboutMeComponent,
     PortfolioContainerComponent,
     ContactComponent,
+    PortfolioItemComponent,
   ],
   imports: [
     BrowserModule
